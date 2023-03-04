9eee6e58678818acfb0fd22b060c837443753137
MohammedShams <=>
2023-03-05 01:28:12 +0530

    Sign In page

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,28 +1,12 @@
-import { useEffect, useState } from 'react';
-import { observable } from '@legendapp/state';
-import { useObserveEffect } from '@legendapp/state/react';
-import { Button, Card, Text } from '@tremor/react';
-
-const state = observable({ count: 0 });
-
-function increment() {
-  state.count.set(prev => ++prev);
-}
+import { lazy } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../src/pages/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Card>
-        <Text>hello world {state.count} </Text>
-
-        <Button
-          marginTop={'mt-3'}
-          onClick={increment}
-        >
-          increment {state.count}
-        </Button>
-      </Card>
-    </div>
+    <>
+      <Login />
+    </>
   );
 }
 
