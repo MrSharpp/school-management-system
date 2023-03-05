19b008140ea60e73875543d1371d0cacbc6fbb95
amiralam0 <sharpprogrammer2018@gmail.com>
2023-03-05 14:18:36 +0530

    swagger init authenmticaiton

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { ZodValidationPipe } from 'nestjs-zod';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestJsSwagger, ZodValidationPipe } from 'nestjs-zod';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AppModule } from './app.module';
 
@@ -8,6 +9,18 @@ async function bootstrap() {
   const primsaService = app.get(PrismaService);
   await primsaService.enableShutdownHooks(app);
   app.useGlobalPipes(new ZodValidationPipe());
+
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle('SMS API')
+    .setVersion('1.0')
+    .build();
+
+  patchNestJsSwagger();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
