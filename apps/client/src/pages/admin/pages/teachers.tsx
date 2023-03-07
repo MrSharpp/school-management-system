3a839fcb5a69cc46e7fe60763079e28e1acc8e1e
amiralam0 <sharpprogrammer2018@gmail.com>
2023-03-08 02:04:46 +0530

    added teachers screen

--- apps/client/src/pages/admin/pages/teachers.tsx
+++ apps/client/src/pages/admin/pages/teachers.tsx
@@ -1,5 +1,64 @@
-import { Text } from '@mantine/core';
+import { Anchor, Breadcrumbs, Button, Container, Flex, Text, Title } from '@mantine/core';
+import {DataTable} from 'mantine-datatable'
 
 export function Teachers() {
-  return <Text>Teacher</Text>;
+    const items = [
+        { title: 'Admin', href: '/' },
+        { title: 'Teachers', href: '/teachers' },
+
+      ].map((item, index) => (
+        <Anchor href={item.href} key={index}>
+          {item.title}
+        </Anchor>
+      ));
+      
+  return (<>
+  <Flex justify={'space-between'} sx={{alignItems: 'center'}} mx={"4%"}>
+  <div><Breadcrumbs separator="/" mt="xs">{items}</Breadcrumbs>
+  <Title mt={4} color={"#495057"}>Teachers</Title></div>
+  <Button mr={"2%"}>Add Teacher</Button>
+  </Flex>
+
+  <DataTable
+  ml={"4%"}
+  mr={"6%"}
+  mt={40}
+  height="80%"
+      withBorder
+      borderRadius="sm"
+      withColumnBorders
+      striped
+      highlightOnHover
+      // provide data
+      records={[
+        { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
+        // more records...
+      ]}
+      // define columns
+      columns={[
+        {
+          accessor: 'id',
+          // this column has a custom title
+          title: '#',
+          // right-align column
+          textAlignment: 'right',
+        },
+        { accessor: 'name' },
+        {
+          accessor: 'party',
+          // this column has custom cell data rendering
+          render: ({ party }) => (
+            <Text weight={700} color={party === 'Democratic' ? 'blue' : 'red'}>
+              {party.slice(0, 3).toUpperCase()}
+            </Text>
+          ),
+        },
+        { accessor: 'bornIn' },
+      ]}
+      // execute this callback when a row is clicked
+      onRowClick={({ name, party, bornIn }) =>
+        alert(`You clicked on ${name}, a ${party.toLowerCase()} president born in ${bornIn}`)
+      }
+    />
+  </>);
 }
