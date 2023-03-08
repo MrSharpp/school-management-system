5e0aefddb10025935acb23981dd9c0d5d43575a3
zakiAzfar <dexk2015@gmail.com>
2023-03-08 18:39:42 +0530

    state to signal migration in all teachers page

--- apps/client/src/pages/admin/pages/Teachers/All Teachers/AllTeachers.tsx
+++ apps/client/src/pages/admin/pages/Teachers/All Teachers/AllTeachers.tsx
@@ -1,156 +1,88 @@
-// import {
-//   Anchor,
-//   Breadcrumbs,
-//   Button,
-//   Container,
-//   Flex,
-//   Text,
-//   Title,
-// } from '@mantine/core';
-// import { DataTable } from 'mantine-datatable';
-// import { Link } from 'react-router-dom';
-
-// export default function Teachers() {
-//   const items = [
-//     { title: 'Admin', href: '/' },
-//     { title: 'Teachers', href: '/teachers' },
-//   ].map((item, index) => (
-//     <Anchor component={Link} to={item.href} key={index}>
-//       {item.title}
-//     </Anchor>
-//   ));
-
-//   return (
-//     <>
-//       <Flex justify={'space-between'} sx={{ alignItems: 'center' }} mx={'4%'}>
-//         <div>
-//           <Breadcrumbs separator="/" mt="xs">
-//             {items}
-//           </Breadcrumbs>
-
-//           <Title mt={4} color={'#495057'}>
-//             Teachers
-//           </Title>
-//         </div>
-//         <Button mr={'2%'}>Add Teacher</Button>
-//       </Flex>
-
-//       <DataTable
-//         ml={'4%'}
-//         mr={'6%'}
-//         mt={40}
-//         height="80%"
-//         withBorder
-//         borderRadius="sm"
-//         withColumnBorders
-//         striped
-//         highlightOnHover
-//         // provide data
-//         records={[
-//           { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-//           // more records...
-//         ]}
-//         // define columns
-//         columns={[
-//           {
-//             accessor: 'id',
-//             // this column has a custom title
-//             title: '#',
-//             // right-align column
-//             textAlignment: 'right',
-//           },
-//           { accessor: 'name' },
-//           {
-//             accessor: 'party',
-//             // this column has custom cell data rendering
-//             render: ({ party }) => (
-//               <Text
-//                 weight={700}
-//                 color={party === 'Democratic' ? 'blue' : 'red'}
-//               >
-//                 {party.slice(0, 3).toUpperCase()}
-//               </Text>
-//             ),
-//           },
-//           { accessor: 'bornIn' },
-//         ]}
-//         // execute this callback when a row is clicked
-//         onRowClick={({ name, party, bornIn }) =>
-//           alert(
-//             `You clicked on ${name}, a ${party.toLowerCase()} president born in ${bornIn}`
-//           )
-//         }
-//       />
-//     </>
-//   );
-// }
-
 import { ActionIcon, Box, Grid, TextInput } from '@mantine/core';
 import { useDebouncedValue } from '@mantine/hooks';
 import { DataTable, DataTableSortStatus } from 'mantine-datatable';
 import { useEffect, useState } from 'react';
 import { IconEdit, IconSearch, IconTrash } from '@tabler/icons-react';
 import { useNavigate } from 'react-router-dom';
-// import sortBy from 'lodash/sortBy';
+import sortBy from 'lodash/sortBy';
+import debounce from 'lodash/debounce';
 import Teachers from './teachers.json';
+import { Container } from '@mantine/core';
+import { observable } from '@legendapp/state';
+import { observer, useObserveEffect } from '@legendapp/state/react';
+import { reactive } from '@legendapp/state/react';
 
 const initialRecords = Teachers.slice(0, 100);
 const PAGE_SIZE = 10;
 
-export default function AllTeachers() {
-  const navigate = useNavigate();
-
-  const [records, setRecords] = useState(initialRecords);
-  const [query, setQuery] = useState('');
-  const [debouncedQuery] = useDebouncedValue(query, 200);
-  const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
+const state = observable({
+  records: initialRecords,
+  query: '',
+  sortStatus: {
     columnAccessor: 'name',
     direction: 'asc',
+  },
+  page: 1,
+  sortedRecords: Teachers.slice(0, PAGE_SIZE),
+});
+
+const debouncedQuery = observable('');
+
+const getDebounceQuery = debounce(() => {
+  debouncedQuery.set(state.query.get());
+}, 300);
+
+state.query.onChange(() => {
+  getDebounceQuery();
+});
+
+const ReactiveTextInput = reactive(TextInput);
+const ReactiveDataTable = reactive(DataTable);
+
+function AllTeachers() {
+  const navigate = useNavigate();
+
+  useObserveEffect(() => {
+    let data = sortBy(Teachers, state.sortStatus.columnAccessor.get());
+    data = state.sortStatus.direction.get() === 'desc' ? data.reverse() : data;
+
+    const from = (state.page.get() - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE;
+
+    const query = debouncedQuery.get();
+    if (query.length) {
+      console.log(query.trim().toLowerCase());
+      console.log(
+        `${Teachers[0].id} ${Teachers[0].name} ${Teachers[0].phone} `
+      );
+
+      data = Teachers.filter(({ id, name, phone }: any) =>
+        `${id} ${name} ${phone}`.match(new RegExp(query.trim(), 'i'))
+      );
+    }
+
+    data = data.slice(from, to);
+
+    state.sortedRecords.set(data);
   });
-  const [page, setPage] = useState(1);
-  const [pageRecords, setPageRecords] = useState(Teachers.slice(0, PAGE_SIZE));
-
-  useEffect(() => {
-    setRecords(
-      initialRecords.filter(({ id, name, phone }: any) => {
-        if (
-          debouncedQuery !== '' &&
-          !`${id} ${name} ${phone} `
-            .toLowerCase()
-            .includes(debouncedQuery.trim().toLowerCase())
-        ) {
-          return false;
-        }
-        return true;
-      })
-    );
-
-    // let data = sortBy(Teachers, sortStatus.columnAccessor);
-    // data = sortStatus.direction === 'desc' ? data.reverse() : data;
-
-    // const from = (page - 1) * PAGE_SIZE;
-    // const to = from + PAGE_SIZE;
-
-    // setPageRecords(data.slice(from, to));
-  }, [debouncedQuery, page, sortStatus]);
 
   return (
-    <>
+    <Container fluid>
       <Grid align="center" mb="md">
         <Grid.Col xs={8} sm={9}>
-          <TextInput
+          <ReactiveTextInput
             sx={{ flexBasis: '60%' }}
             placeholder="Search teachers..."
             icon={<IconSearch size={16} />}
-            value={query}
-            onChange={(e) => setQuery(e.currentTarget.value)}
+            value$={state.query}
+            onChange={(e) => state.query.set(e.currentTarget.value)}
           />
         </Grid.Col>
       </Grid>
 
-      <DataTable
+      <ReactiveDataTable
         withBorder
-        records={records}
+        records$={state.sortedRecords}
         columns={[
           {
             accessor: 'id',
@@ -182,16 +114,17 @@ export default function AllTeachers() {
                   style={{
                     display: 'flex',
                     gap: 10,
-                    justifyContent: 'center',
+                    // justifyContent: 'center',
                   }}
                 >
                   <ActionIcon
-                    color="dark"
+                    color="gray"
                     onClick={() => navigate('patient-details')}
                   >
                     <IconEdit size={16} />
                   </ActionIcon>
-                  <ActionIcon>
+
+                  <ActionIcon color="red" >
                     <IconTrash size={16} />
                   </ActionIcon>
                 </div>
@@ -199,13 +132,16 @@ export default function AllTeachers() {
             },
           },
         ]}
-        sortStatus={sortStatus}
-        onSortStatusChange={setSortStatus}
+        sortStatus$={state.sortStatus}
+        onSortStatusChange={state.sortStatus.set}
         totalRecords={Teachers.length}
         recordsPerPage={PAGE_SIZE}
-        page={page}
-        onPageChange={(p) => setPage(p)}
+        page$={state.page}
+        onPageChange={(p) => state.page.set(p)}
       />
-    </>
+    </Container>
   );
 }
+
+// export default observer(AllTeachers);
+export default AllTeachers;
