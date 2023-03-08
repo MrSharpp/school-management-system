02b03986559400e1c7be2712a92a6e22b570017c
amiralam0 <sharpprogrammer2018@gmail.com>
2023-03-09 00:50:37 +0530

    all students integrated

--- apps/client/src/pages/admin/students/AllStudents.tsx
+++ apps/client/src/pages/admin/students/AllStudents.tsx
@@ -1,71 +1,88 @@
 import {
   ActionIcon,
+  Box,
   Grid,
-  TextInput,
   Flex,
   Breadcrumbs,
   Title,
   Button,
   Anchor,
   Container,
+  Group,
 } from '@mantine/core';
-import { useDebouncedValue } from '@mantine/hooks';
 import { DataTable, DataTableSortStatus } from 'mantine-datatable';
-import { useEffect, useState } from 'react';
-import { IconEdit, IconSearch, IconTrash } from '@tabler/icons-react';
+import { IconEdit, IconSearch, IconTrash, IconPlus } from '@tabler/icons-react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-// import sortBy from 'lodash/sortBy';
-import Students from './students.json';
+import sortBy from 'lodash/sortBy';
+import debounce from 'lodash/debounce';
+import Teachers from './students.json';
+import { observable } from '@legendapp/state';
+import { observer, useObserveEffect } from '@legendapp/state/react';
+import { TextInput$, DataTable$ } from 'ui';
+import { useObservableQuery } from '@legendapp/state/react-hooks/useObservableQuery';
+import ApiCalls from '@APIService/index';
 
-const initialRecords = Students.slice(0, 100);
+const initialRecords = Teachers.slice(0, 100);
 const PAGE_SIZE = 10;
 
-const items = [
-  { title: 'Admin', href: '/' },
-  { title: 'Students', href: '/students' },
-].map((item, index) => (
-  <Anchor component={Link} to={item.href} key={index}>
-    {item.title}
-  </Anchor>
-));
-
-export default function AllStudents() {
-  const navigate = useNavigate();
-
-  const [records, setRecords] = useState(initialRecords);
-  const [query, setQuery] = useState('');
-  const [debouncedQuery] = useDebouncedValue(query, 200);
-  const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
+const state = observable({
+  records: initialRecords,
+  query: '',
+  sortStatus: {
     columnAccessor: 'name',
     direction: 'asc',
+  },
+  page: 1,
+  sortedRecords: Teachers.slice(0, PAGE_SIZE),
+});
+
+const debouncedQuery = observable('');
+
+const getDebounceQuery = debounce(() => {
+  debouncedQuery.set(state.query.get());
+}, 300);
+
+state.query.onChange(() => {
+  getDebounceQuery();
+});
+
+export default function AllTeachers() {
+  const navigate = useNavigate();
+
+  const getTeachersQuery = useObservableQuery({
+    queryKey: ['get_students'],
+    queryFn: ApiCalls.getStudents,
   });
-  const [page, setPage] = useState(1);
-  const [pageRecords, setPageRecords] = useState(Students.slice(0, PAGE_SIZE));
 
-  useEffect(() => {
-    setRecords(
-      initialRecords.filter(({ id, name, phone }: any) => {
-        if (
-          debouncedQuery !== '' &&
-          !`${id} ${name} ${phone} `
-            .toLowerCase()
-            .includes(debouncedQuery.trim().toLowerCase())
-        ) {
-          return false;
-        }
-        return true;
-      })
-    );
+  const items = [
+    { title: 'Admin', href: '/' },
+    { title: 'Students', href: '/students' },
+  ].map((item, index) =>
+    <Anchor component={Link} to={item.href} key={index}>
+      {item.title}
+    </Anchor>
+  );
 
-    // let data = sortBy(Teachers, sortStatus.columnAccessor);
-    // data = sortStatus.direction === 'desc' ? data.reverse() : data;
+  useObserveEffect(() => {
+    let data = [...Teachers];
 
-    // const from = (page - 1) * PAGE_SIZE;
-    // const to = from + PAGE_SIZE;
+    const query = debouncedQuery.get().trim().toLowerCase();
 
-    // setPageRecords(data.slice(from, to));
-  }, [debouncedQuery, page, sortStatus]);
+    if (query.length) {
+      data = data.filter(({ id, name, phone }: any) =>
+        `${id} ${name} ${phone} `.toLowerCase().includes(query)
+      );
+    }
+
+    data = sortBy(data, state.sortStatus.columnAccessor.get());
+    data = state.sortStatus.direction.get() === 'desc' ? data.reverse() : data;
+
+    const from = (state.page.get() - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE;
+
+    state.sortedRecords.set(data.slice(from, to));
+  });
 
   return (
     <Container fluid>
@@ -81,48 +98,55 @@ export default function AllStudents() {
                 Students
               </Title>
             </div>
-            <Button mr={'1%'}>Add Student</Button>
+            <Button mr={'1%'} onClick={() => navigate('new')}>
+              Add Student
+            </Button>
           </Flex>
         </Grid.Col>
-        <Grid.Col xs={2} sm={2}>
-          <TextInput
-            sx={{ flexBasis: '60%' }}
+
+        <Grid.Col xs={3} sm={3}>
+          <TextInput$
             placeholder="Search teachers..."
             icon={<IconSearch size={16} />}
-            value={query}
-            onChange={(e) => setQuery(e.currentTarget.value)}
+            value$={state.query}
+            onChange={e => state.query.set(e.currentTarget.value)}
           />
         </Grid.Col>
       </Grid>
-
-      <DataTable
-        horizontalSpacing="md"
-        verticalSpacing="md"
-        fontSize="md"
-        highlightOnHover
-        striped
-        records={records}
+      {console.log(getTeachersQuery.get())}
+      <DataTable$
+        withBorder
+        // records$={getTeachersQuery.data || []}
+        records={getTeachersQuery.data || []}
+        fetching$={getTeachersQuery.isLoading}
+        idAccessor="teacherId"
         columns={[
           {
-            accessor: 'id',
-            sortable: true,
+            accessor: 'studentId',
+            // sortable: true,
           },
           {
             accessor: 'name',
-            sortable: true,
-          },
-          { accessor: 'class', sortable: true },
-          { accessor: 'gender', sortable: true },
-          {
-            accessor: 'subject',
-            sortable: true,
+            // sortable: true,
           },
           {
-            accessor: 'section',
-            sortable: true,
+            accessor: 'email',
+            //  sortable: true
           },
-          { accessor: 'phone', title: 'Phone Number', sortable: true },
-          { accessor: 'address', sortable: true },
+          {
+            accessor: 'rollNo',
+
+            //  sortable: true
+          },
+          {
+            accessor: 'guardianNumber',
+
+            //  sortable: true
+          },
+          {
+            accessor: 'gender',
+            //  sortable: true
+          },
           {
             accessor: 'action',
             width: '10%',
@@ -142,7 +166,8 @@ export default function AllStudents() {
                   >
                     <IconEdit size={16} />
                   </ActionIcon>
-                  <ActionIcon>
+
+                  <ActionIcon color="red">
                     <IconTrash size={16} />
                   </ActionIcon>
                 </div>
@@ -150,13 +175,16 @@ export default function AllStudents() {
             },
           },
         ]}
-        sortStatus={sortStatus}
-        onSortStatusChange={setSortStatus}
-        totalRecords={Students.length}
-        recordsPerPage={PAGE_SIZE}
-        page={page}
-        onPageChange={(p) => setPage(p)}
+        // sortStatus$={state.sortStatus}
+        // onSortStatusChange={state.sortStatus.set}
+        // totalRecords$={state.records.length}
+        totalRecords$={getTeachersQuery.data.length}
+        // recordsPerPage={PAGE_SIZE}
+        // page$={state.page}
+        // onPageChange={p => state.page.set(p)}
       />
     </Container>
   );
 }
+
+// export default AllTeachers
